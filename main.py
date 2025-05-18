e9d969c053d0ff5934917b6bf0eae94f65402b93
Yalçın Tür <yalcinturpg@gmail.com>
2025-05-17 19:28:53 -0700

    change

--- main.py
+++ main.py
@@ -38,21 +38,22 @@ if __name__ == "__main__":
     # TODO: change to the list of objects
     list_of_objects = ["orange", "apple", "sports ball", "cup"]  
     
-    # TODO: change to the image path
+    p_img = run_command(f"python3 send_d405_images.py")
+    
     llama = LlamaPrompt(image_path="/home/cs225a1/ina/8VC-Hackathon/d405_image_10.png")  
     result = llama.prompt_llama(list_of_objects)
     print(result)
     
+
     # Process each object
     for object_name, bag_name in result:
-        # Start the appropriate visual servoing demo based on bag classification
+        p1 = run_command(f"python3 yolo_servo_perception.py -c {object_name}")
+        
         if bag_name == "food":
-            p1 = run_command("python3 visual_servoing_demo.py -A")
+            p2 = run_command("python3 visual_servoing_demo.py -A")
         elif bag_name == "non-food":
-            p1 = run_command("python3 visual_servoing_demo.py -B")
+            p2 = run_command("python3 visual_servoing_demo.py -B")
             
-        # Start the perception script for the specific object
-        p2 = run_command(f"python3 yolo_servo_perception.py -c {object_name}")
         
         # Wait for both processes to complete before moving to the next object
         print(f"Processing object: {object_name}...")
